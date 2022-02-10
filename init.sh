2808b662e8d1d966ee015d6c18f8fbd5f97ac5c9
Robin <rob@Robins-MacBook-Air.local>
2022-02-10 13:44:52 +0100

    added local ingress

--- init.sh
+++ init.sh
@@ -2,6 +2,7 @@
 kubectl apply -f jenkins-namespace.yaml
 kubectl apply -f jenkins-volume.yaml
 kubectl apply -f jenkins-sa.yaml
+kubectl apply -f jenkins-ingress.yaml
 helm repo add jenkinsci https://charts.jenkins.io
 helm repo update
 helm install jenkins -n jenkins -f jenkins-values.yaml jenkinsci/jenkins
--- upgrade_values.sh
+++ upgrade_values.sh
@@ -2,4 +2,6 @@
 kubectl apply -f jenkins-namespace.yaml
 kubectl apply -f jenkins-volume.yaml
 kubectl apply -f jenkins-sa.yaml
+kubectl apply -f jenkins-ingress.yaml
+helm repo update
 helm upgrade jenkins -n jenkins -f jenkins-values.yaml jenkinsci/jenkins
